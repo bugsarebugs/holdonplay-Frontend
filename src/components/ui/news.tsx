9cd0097546b9bd41f621e3a6d618a912ab0313de
bugsarebugs <joehacker9273@proton.me>
2025-10-23 00:52:54 -0400

    First Commit

---
+++ src/components/ui/news.tsx
@@ -0,0 +1,20 @@
+import { FaStar, FaSun, FaRocket } from "react-icons/fa"
+
+export default function News(){
+    return (
+        <div className=" text-blue-500  overflow-hidden">
+            <div className=" flex flex-row gap-4 flex-nowrap scrollme  overflow-x-scroll w-1900">
+                <span className="">HOP</span>
+                <span className=""><FaSun size={20} className="text-blue-500"/></span>
+                <span className="">hold ON play</span>
+                <span className="">Your Music</span>
+                <span className="">Distribution Company</span>
+                <span className=""><FaRocket size={20} className="text-blue-500"/></span>
+                <span className="">We hav our ways</span>
+                <span className="">Music is Power, Amplify It</span>
+                <span className="">Make sure you plug in those Headphones</span>
+                <span className=""><FaStar size={20} className="text-blue-500"/></span>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/ui/secondSection.tsx
@@ -0,0 +1,54 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const logoz = ["/spotify.webp", "/tidal.svg", "/youtube.svg", "/beatport.svg", "/amazon.svg", "/apple.svg"]
+export default function SecondSection(){
+    const boxRef = useRef(null);
+    useGSAP(() => {
+        
+        gsap.fromTo(
+        boxRef.current,
+        { opacity: 0.5, x: 300 , duration: 20}, 
+        
+        {
+            opacity: 1, 
+            x: 0,
+            duration: 10,
+            ease: "power2.out", 
+            ScrollTrigger: {
+                trigger: boxRef.current, 
+                start: "top 80%", 
+                end: "top 50%",
+                scrub: true
+            }
+        }
+    );
+    return () => ScrollTrigger.killAll();
+    }, [])
+;    return (
+        <div ref={boxRef} className="bg-black flex relative flex-col justify-center items-center  p-4 text-center text-2xl ">
+            <div className="flex flex-col lg:px-100">
+            <h1 className="lg:text-6xl font-bold text-4xl"><span className="text-blue-400">Unlimited Distribution</span> Starting at Tsh: 77,000/=</h1>
+            <p className="text-justify">Increase the reach of your music across the most popular stores & platforms like Spotify, Apple Music, TikTok, Youtube and many more. Empower yourself with unlimited distribution opportunities, and get your music heard by a global audience</p>
+            <p className="text-justify">Keep 100% ownership of your music, maintaining creative control and authority in your music career</p>
+            <div className="flex self-center flex-wrap items-center justify-center p-4">{logoz.map((src) => (
+                <Image src={src} key={src} width={200} height={300} alt="logos" className="size-20 lg:size-20 p-1 "/>
+            ))}
+            </div>
+            <button className="border-2 rounded-full p-2 border-blue-400 bg-blue-400 hover:bg-blue-800 w-fit self-center">
+                <Link href="/stores">
+                    VIEW ALL 150+ StORES
+                </Link>
+            </button>
+            </div>
+          
+        </div>
+    )
+}
